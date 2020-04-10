8df41e63493f9b68fb7ef5f362fc7c9127d34e57
ivnard <ivan.nardini@sas.com>
2020-04-10 18:04:05 +0200

    I don't like disk parameter :) and I like create conflict

--- all_checks.py
+++ all_checks.py
@@ -27,7 +27,7 @@ def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
-    if check_disk_full(disk="/", min_gb=2, min_percent=10):
+    if check_disk_full("/", min_gb=2, min_percent=10):
         print('Disk full.')
         sys.exit(1)
 
